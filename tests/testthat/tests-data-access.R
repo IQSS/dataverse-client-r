90c8408d3cab7a94a7d80759fd93c0d7d3f1bedd
Thomas J. Leeper <thosjleeper@gmail.com>
2017-06-13 15:08:21 +0100

    update tests

--- tests/testthat/tests-data-access.R
+++ tests/testthat/tests-data-access.R
@@ -1,21 +1,25 @@
 context("Data Access API")
 
-test_that("get file metadata from DOI and filename", {
-    f1 <- get_metadata("constructionData.tab", "doi:10.7910/DVN/ARKOTI", server = "dataverse.harvard.edu")
-    expect_true(is.list(f1))
-})
+if (Sys.getenv("DATAVERSE_KEY") != "") {
 
-test_that("get file metadata from file id", {
-    f1 <- get_metadata(2692202, server = "dataverse.harvard.edu")
-    expect_true(is.list(f1))
-})
+    test_that("get file metadata from DOI and filename", {
+        f1 <- get_file_metadata("constructionData.tab", "doi:10.7910/DVN/ARKOTI", server = "dataverse.harvard.edu")
+        expect_true(is.list(f1))
+    })
 
-test_that("download file from DOI and filename", {
-    f1 <- get_file("constructionData.tab", "doi:10.7910/DVN/ARKOTI", server = "dataverse.harvard.edu")
-    expect_true(is.raw(f1))
-})
+    test_that("get file metadata from file id", {
+        f1 <- get_file_metadata(2692202, server = "dataverse.harvard.edu")
+        expect_true(is.list(f1))
+    })
 
-test_that("download file from file id", {
-    f2 <- get_file(2692202, server = "dataverse.harvard.edu")
-    expect_true(is.raw(f2))
-})
+    test_that("download file from DOI and filename", {
+        f1 <- get_file("constructionData.tab", "doi:10.7910/DVN/ARKOTI", server = "dataverse.harvard.edu")
+        expect_true(is.raw(f1))
+    })
+
+    test_that("download file from file id", {
+        f2 <- get_file(2692202, server = "dataverse.harvard.edu")
+        expect_true(is.raw(f2))
+    })
+
+}
