f264f93683711f38db2f74ba13e6a81a2f450b30
Thomas J. Leeper <thosjleeper@gmail.com>
2017-06-15 07:46:26 -0400

    tweak tests (#4)

--- tests/testthat/tests-data-access.R
+++ tests/testthat/tests-data-access.R
@@ -2,12 +2,12 @@ context("Data Access API")
 
 test_that("get file metadata from DOI and filename", {
     f1 <- get_file_metadata("constructionData.tab", "doi:10.7910/DVN/ARKOTI", server = "dataverse.harvard.edu")
-    expect_true(is.list(f1))
+    expect_true(is.character(f1))
 })
 
 test_that("get file metadata from file id", {
-    f1 <- get_file_metadata(2692202, server = "dataverse.harvard.edu")
-    expect_true(is.list(f1))
+    f1 <- get_file_metadata(2692293, server = "dataverse.harvard.edu")
+    expect_true(is.character(f1))
 })
 
 test_that("download file from DOI and filename", {
@@ -16,6 +16,6 @@ test_that("download file from DOI and filename", {
 })
 
 test_that("download file from file id", {
-    f2 <- get_file(2692202, server = "dataverse.harvard.edu")
-    expect_true(is.raw(f2))
+    f1 <- get_file(2692293, server = "dataverse.harvard.edu")
+    expect_true(is.raw(f1))
 })
