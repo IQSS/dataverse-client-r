409fd5bd20e46bffda8b5f951302c8af70fa862a
Sebastian Karcher <karcher@u.northwestern.edu>
2019-12-20 22:27:22 -0500

    add test for fq date range search

--- tests/testthat/tests-search.R
+++ tests/testthat/tests-search.R
@@ -11,3 +11,7 @@ test_that("named argument search", {
 test_that("simple search w/type argument", {
     expect_true(is.data.frame(dataverse_search(author = "Gary King", type = "dataset", key = "", server = "dataverse.harvard.edu")))
 })
+
+test_that("date range search using fq", {
+    expect_true(is.data.frame(dataverse_search(author = "*", fq = "dateSort:[2018-01-01T00:00:00Z+TO+2019-01-01T00:00:00Z]", type = "dataset", key = "", server = "dataverse.harvard.edu")))
+})
