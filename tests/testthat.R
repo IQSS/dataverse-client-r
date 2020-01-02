51047830892f5478ad4901895127945efe6f99a0
Will Beasley <wibeasley@hotmail.com>
2020-01-01 23:01:20 -0500

    read Dataverse token from yaml --test should fail!
    ref #40

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,7 +1,33 @@
 library("testthat")
 library("dataverse")
 
-key <- Sys.getenv("DATAVERSE_KEY")
-if (!is.null(key) && key != "") {
-    test_check("dataverse")
+if (requireNamespace("yaml", quietly = TRUE)) {
+  server  <- Sys.getenv("DATAVERSE_SERVER")
+  key     <- Sys.getenv("DATAVERSE_KEY")
+
+  if (server == "" | key == "") {
+    config  <- yaml::read_yaml(system.file("constants.yml", package="dataverse"))
+    # config  <- yaml::read_yaml("inst/constants.yml")
+
+    Sys.setenv("DATAVERSE_SERVER" = config$server)
+    Sys.setenv("DATAVERSE_KEY"    = config$api_token)
+
+    if (as.Date(config$api_token_expiration) < Sys.Date()) {
+      stop(
+        "The API token expired on `",
+        config$api_token_expiration,
+        "`, so the tests would probably fail.  ",
+        "Please regenerate a new token and update `inst/constants.yml`"
+      )
+    }
+    rm(config)
+  }
+  rm(server, key)
+
+  message("Using Dataverse server `", Sys.getenv("DATAVERSE_SERVER"), "`.")
+
+  test_check("dataverse")
+
+} else {
+  warning("The 'yaml' package must be present to test the dataverse package.")
 }
