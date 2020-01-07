03fb51507c8d8ae9a726477d9e24e168b3ce20a3
adam3smith <karcher@u.northwestern.edu>
2020-01-07 10:40:05 -0500

    More fq tests

--- tests/testthat/tests-search.R
+++ tests/testthat/tests-search.R
@@ -16,6 +16,14 @@ test_that("date range search using fq", {
     expect_true(is.data.frame(dataverse_search("*", fq = "dateSort:[2018-01-01T00:00:00Z+TO+2019-01-01T00:00:00Z]", type = "dataset", key = "", server = "dataverse.harvard.edu")))
 })
 
+test_that("publication year using fq", {
+    expect_true(is.data.frame(dataverse_search("*", fq = "publicationDate:2018", type = "dataset", key = "", server = "dataverse.harvard.edu")))
+})
+
+test_that("filter dataverses by subject using fq", {
+    expect_true(is.data.frame(dataverse_search("*", fq = "subject_ss:Social+Sciences", type = "dataverse", key = "", server = "dataverse.harvard.edu")))
+})
+
 test_that("empty fq search", {
-  expect_length(dataverse_search("*", fq = "dateSort:[2019-02-01T00:00:00Z+TO+2019-01-01T00:00:00Z]", type = "dataset", key = "", server = "dataverse.harvard.edu"), 0)
+    expect_length(dataverse_search("*", fq = "dateSort:[2019-02-01T00:00:00Z+TO+2019-01-01T00:00:00Z]", type = "dataset", key = "", server = "dataverse.harvard.edu"), 0)
 })
