ff7e87ce67ab74e4485026b508b982cd903e83d2
Will Beasley <wibeasley@hotmail.com>
2020-02-24 17:23:27 -0600

    tidy
    ref #4

--- tests/testthat/tests-get_file.R
+++ tests/testthat/tests-get_file.R
@@ -3,7 +3,7 @@ context("Data Access API")
 # See https://demo.dataverse.org/dataverse/dataverse-client-r
 # https://doi.org/10.70122/FK2/FAN622
 
-test_that("download file from DOI and filename", {
+test_that("download tab from DOI and filename", {
   actual <- get_file(
     file = "roster-bulls-1996.tab",
     dataset = "doi:10.70122/FK2/FAN622"
@@ -12,7 +12,7 @@ test_that("download file from DOI and filename", {
   expect_true(1000 < object.size(actual)) # Should be 1+ KB
 })
 
-test_that("download file from file id", {
+test_that("download tab from file id", {
   actual <- get_file(
     file = 396357
   )
