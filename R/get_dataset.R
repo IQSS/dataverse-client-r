2ed1a1161d0263d87d68b2f2f2888ab563976165
Will Beasley <wibeasley@hotmail.com>
2020-03-20 01:44:10 -0500

    `data_files()`, `dataset_metadata()`, and `get_dataset()` simple tests & examples
    ref #4

--- R/get_dataset.R
+++ R/get_dataset.R
@@ -1,7 +1,19 @@
 #' @rdname get_dataset
 #' @title Get dataset
 #' @description Retrieve a Dataverse dataset or its metadata
-#' @details \code{get_dataset} retrieves details about a Dataverse dataset. \code{dataset_metadata} returns a named metadata block for a dataset. This is already returned by \code{\link{get_dataset}}, but this function allows you to retrieve just a specific block of metadata, such as citation information. \code{dataset_files} returns a list of files in a dataset, similar to \code{\link{get_dataset}}. The difference is that this returns only a list of \dQuote{dataverse_dataset} objects, whereas \code{\link{get_dataset}} returns metadata and a data.frame of files (rather than a list of file objects).
+#'
+#' @details
+#' \code{get_dataset} retrieves details about a Dataverse dataset.
+#'
+#' \code{dataset_metadata} returns a named metadata block for a dataset.
+#' This is already returned by \code{\link{get_dataset}}, but this function allows
+#' you to retrieve just a specific block of metadata, such as citation information.
+#'
+#' \code{dataset_files} returns a list of files in a dataset, similar to
+#' \code{\link{get_dataset}}. The difference is that this returns only a list of
+#' \dQuote{dataverse_dataset} objects, whereas \code{\link{get_dataset}} returns
+#' metadata and a data.frame of files (rather than a list of file objects).
+#'
 #' @template ds
 #' @template version
 #' @template envvars
@@ -9,13 +21,28 @@
 #' @return A list of class \dQuote{dataverse_dataset} or a list of a form dependent on the specific metadata block retrieved. \code{dataset_files} returns a list of objects of class \dQuote{dataverse_file}.
 #' @examples
 #' \dontrun{
-#' # download file from:
-#' # https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/ARKOTI
-#' monogan <- get_dataverse("monogan")
-#' monogan_data <- dataverse_contents(monogan)
-#' d1 <- get_dataset(monogan_data[[1]])
-#' dataset_files(monogan_data[[1]])
-#' f <- get_file(d1$files$datafile$id[3])
+#' Sys.setenv("DATAVERSE_SERVER" = "demo.dataverse.org")
+#' Sys.setenv("DATAVERSE_KEY"    = "c7208dd2-6ec5-469a-bec5-f57e164888d4")
+#'
+#' # Download file from: https://demo.dataverse.org/file.xhtml?fileId=769385
+#' dv       <- get_dataverse("dataverse-client-r")
+#' contents <- dataverse_contents(dv)
+#'
+#' dataset_files(contents[[1]])            # Dataset contains 2 files
+#' dataset_metadata(contents[[1]])         # Easier to query later
+#'
+#' set      <- get_dataset(contents[[1]])  # 1st dataset w/n dataverse
+#' f        <- get_file(set$files$id[2])   # 2nd file w/n dataset
+#'
+#' # Check the *binary* representation of the file.
+#' length(f)
+#' head(f)
+#'
+#' # Examine the plain-text representation.
+#' tmp <- tempfile(fileext = "svg")
+#' writeBin(as.vector(f), tmp)
+#' svg_lines <- readLines(tmp)
+#' head(svg_lines)
 #' }
 #' @seealso \code{\link{create_dataset}}, \code{\link{update_dataset}}, \code{\link{delete_dataset}}, \code{\link{publish_dataset}}, \code{\link{dataset_files}}, \code{\link{dataset_metadata}}
 #' @export
@@ -49,7 +76,7 @@ dataset_metadata <- function(
   server      = Sys.getenv("DATAVERSE_SERVER"),
   ...
  ) {
-dataset <- dataset_id(dataset, key = key, server = server, ...)
+  dataset <- dataset_id(dataset, key = key, server = server, ...)
   if (!is.null(block)) {
     u <- paste0(api_url(server), "datasets/", dataset, "/versions/", version, "/metadata/", block)
   } else {
