56feab169672caea8b9c9fd6d5ee17349be93822
Will Beasley <wibeasley@hotmail.com>
2020-12-31 17:52:38 -0600

    include doi & id retrieval
    ref #70

--- tests/testthat/tests-get_dataframe.R
+++ tests/testthat/tests-get_dataframe.R
@@ -1,14 +1,14 @@
 # See https://demo.dataverse.org/dataverse/dataverse-client-r
 # https://doi.org/10.70122/FK2/HXJVJU
 
-test_that("roster-original", {
+test_that("roster-original-by-name", {
   expected_ds <- retrieve_info_dataset("dataset-basketball/expected-metadata.yml")
   expected_file <- expected_ds$roster$raw_value
 
   actual <-
     get_dataframe_by_name(
-      filename = expected_ds$roster$label , #"roster-bulls-1996.tab",
-      dataset  = dirname(expected_ds$roster$dataFile$persistentId), #"doi:10.70122/FK2/HXJVJU",
+      filename = expected_ds$roster$label , # A value like "roster-bulls-1996.tab",
+      dataset  = dirname(expected_ds$roster$dataFile$persistentId), # A value like "doi:10.70122/FK2/HXJVJU",
       original = TRUE,
       FUN      = readr::read_file
     )
@@ -19,7 +19,41 @@ test_that("roster-original", {
   expect_equal(actual, expected_file)
 })
 
-test_that("image-original", {
+test_that("roster-original-by-doi", {
+  expected_ds <- retrieve_info_dataset("dataset-basketball/expected-metadata.yml")
+  expected_file <- expected_ds$roster$raw_value
+
+  actual <-
+    get_dataframe_by_doi(
+      filedoi  = expected_ds$roster$dataFile$persistentId, # A value like "doi:10.70122/FK2/HXJVJU/SA3Z2V",
+      original = TRUE,
+      FUN      = readr::read_file
+    )
+
+  expect_equal(substr(actual, 1, 30), substr(expected_file, 1, 30))
+  expect_equal(nchar( actual       ), nchar( expected_file       ))
+
+  expect_equal(actual, expected_file)
+})
+
+test_that("roster-original-by-id", {
+  expected_ds <- retrieve_info_dataset("dataset-basketball/expected-metadata.yml")
+  expected_file <- expected_ds$roster$raw_value
+
+  actual <-
+    get_dataframe_by_id(
+      fileid   = expected_ds$roster$dataFile$id, # A value like 1734005
+      original = TRUE,
+      FUN      = readr::read_file
+    )
+
+  expect_equal(substr(actual, 1, 30), substr(expected_file, 1, 30))
+  expect_equal(nchar( actual       ), nchar( expected_file       ))
+
+  expect_equal(actual, expected_file)
+})
+
+test_that("image-original-by-name", {
   expected_ds <- retrieve_info_dataset("dataset-basketball/expected-metadata.yml")
   expected_file <- expected_ds$image$raw_value
 
@@ -27,6 +61,40 @@ test_that("image-original", {
     get_dataframe_by_name(
       filename = expected_ds$image$label , #"vector-basketball.svg",
       dataset  = dirname(expected_ds$image$dataFile$persistentId), #"doi:10.70122/FK2/HXJVJU",
+      original = TRUE,
+      FUN      = readr::read_file
+    )
+
+  expect_equal(substr(actual, 1, 30), substr(expected_file, 1, 30))
+  expect_equal(nchar( actual       ), nchar( expected_file       ))
+
+  expect_equal(actual, expected_file)
+})
+
+test_that("image-original-by-doi", {
+  expected_ds <- retrieve_info_dataset("dataset-basketball/expected-metadata.yml")
+  expected_file <- expected_ds$image$raw_value
+
+  actual <-
+    get_dataframe_by_doi(
+      filedoi  = expected_ds$image$dataFile$persistentId, # A value like "doi:10.70122/FK2/HXJVJU/FHV8ZB",
+      original = TRUE,
+      FUN      = readr::read_file
+    )
+
+  expect_equal(substr(actual, 1, 30), substr(expected_file, 1, 30))
+  expect_equal(nchar( actual       ), nchar( expected_file       ))
+
+  expect_equal(actual, expected_file)
+})
+
+test_that("image-original-by-id", {
+  expected_ds <- retrieve_info_dataset("dataset-basketball/expected-metadata.yml")
+  expected_file <- expected_ds$image$raw_value
+
+  actual <-
+    get_dataframe_by_id(
+      fileid   = expected_ds$image$dataFile$id, # A value like 1734006
       original = TRUE,
       FUN      = readr::read_file
     )
