5153bb212c316a9c650b1b86cecb9bef610691ce
Beniamino Green <ben@greendalba.com>
2024-08-09 15:41:40 -0400

    adding caching for

--- R/onload.R
+++ R/onload.R
@@ -1,6 +1,16 @@
+#' @importFrom memoise memoise
+#' @importFrom rappdirs user_cache_dir
 .onLoad <- function(libname, pkgname) {
-    # a <- Sys.getenv("DATAVERSE_SERVER")
-    # if(a == "") {
-    #     Sys.setenv("DATAVERSE_SERVER" = "dataverse.harvard.edu")
-    # }
+  disk_cache <- cachem::cache_disk(rappdirs::user_cache_dir("R-dataverse-client"))
+
+  memoised_get_file_by_id <- memoise::memoise(get_file_by_id, cache = disk_cache)
+  non_memoised_get_file_by_id <- get_file_by_id
+
+  get_file_by_id  <<- function(...) {
+      if ("version" %in% names(list(...))) {
+          memoised_get_file_by_id(...)
+      } else {
+          non_memoised_get_file_by_id(...)
+      }
+  }
 }
