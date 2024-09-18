14a082d2621e404e30cdbc4ef848c8913df305cd
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-09-17 22:08:02 -0400

    Fix a missing quote (typo). Also simplify example for dataset argument -- the"doi:" prefix is not necessary

--- man-roxygen/envvars.R
+++ man-roxygen/envvars.R
@@ -7,5 +7,5 @@
 #'   most major. The server can be defined each time within a function, or it can
 #'   be set as a default via an environment variable. To set a default, run
 #'   `Sys.setenv("DATAVERSE_SERVER" = "dataverse.harvard.edu")`
-#'   or add `DATAVERSE_SERVER = "dataverse.harvard.edu` in one's `.Renviron`
+#'   or add `DATAVERSE_SERVER = "dataverse.harvard.edu"` in one's `.Renviron`
 #'   file (`usethis::edit_r_environ()`), with the appropriate domain as its value.
