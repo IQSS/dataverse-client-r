e5e32848ae92263bc86c7c6f12952638b96ba205
Shiro Kuriwaki <shirokuriwaki@gmail.com>
2024-10-16 14:48:16 -0400

    Make version argument a bit clearer

--- man-roxygen/version.R
+++ man-roxygen/version.R
@@ -1,1 +1,7 @@
-#' @param version A character string specifying a version of the dataset. This can be one of \dQuote{:draft} (the current draft), \dQuote{:latest} (the latest draft, if it exists, or the latest published version), \dQuote{:latest-published} (the latest published version, ignoring any draft), or \dQuote{x.y} (where \samp{x} is a major version and \samp{y} is a minor version; the \samp{.y} can be omitted to obtain a major version). In lieu of this, a dataset's version-specific identification number can be used for the \code{dataset} argument. 
+#' @param version A character string specifying a version of the dataset.
+#'  This can be of the form `"1.1"` or `"1"` (where in `"x.y"`, x is a major version and y is an optional minor version),
+#'  `":latest"` (the latest draft, if it exists, or the latest published version),
+#'  `":latest-published"` (the latest published version, ignoring any draft), or
+#'   `":draft"` (the current draft),
+#'  In lieu of this, a dataset's version-specific identification number can be used for the \code{dataset} argument.
+#'  We recommend using the number format so that the function stores a cache of the data (See \link{cache}).
